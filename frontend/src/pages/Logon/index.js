2c8e9cf457693633df5867c3f7740bb96a19b415
jhersonharyson <jherson_k-f@hotmail.com>
2020-03-29 16:18:10 -0300

    longo integration with backend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
+import api from '../../services/api'
 
 import './style.css'
 
@@ -8,12 +9,48 @@ import heroesImage from '../../assets/heroes.png'
 import logoImage from '../../assets/logo.svg'
 
 export default function Logon() {
+    
+    const [id, setId] = useState('')
+
+    const history = useHistory()
+
+    const login = async (ong_id) =>{
+        try{
+            const response = await api.post('/session', { id })
+            if(response.status == 204){
+                localStorage.setItem('ONG_ID', id)
+                localStorage.setItem('ONG_NAME', response.data.name)
+                history.push('/profile')
+            }
+        }catch(e){
+            alert('Erro ao tentar logar.')
+        }
+    }
+
+    useEffect(()=>{
+        const id = localStorage.getItem('ONG_ID')
+        if(id)
+           login(id)
+    },[])
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        login(id)
+    }
+
+    
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="Be the hero"/>
-                <form>
-                    <input placeholder="Informe seu ID" required></input>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        placeholder="Informe seu ID" 
+                        required
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button">Entrar</button> 
                     <Link className="link-redirect" to="/register"><FiLogIn size={16} color="E02041"/>Não tenho cadastro</Link>
                 </form>
